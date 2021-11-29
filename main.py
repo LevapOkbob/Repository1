9a8e32a54d839cbd58bf4eaaa38ce97af87588d2
Peter <zaz_99vk@mail.ru>
2021-11-29 22:03:33 +0300

    Modified main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 
 def decorator(function):
-    def wrapper():
+    def wrapper(*args):
         start_time = time.time()
         function()
         print("--- %s seconds ---" % (time.time() - start_time))
@@ -12,21 +12,24 @@ def decorator(function):
 
 
 @decorator
-def landarea():
+def landarea(*args):
     ac = 0.000022957
-    landheight = input("Введите длину участка в футах: ")
-    landwidth = input("Введите ширину участка в футах: ")
     area = float(landheight) * float(landwidth) * float(ac)
     print("Площадь участка земли в акрах: " + str(area))
 
 
 @decorator
-def freefall():
+def freefall(*args):
     g = 9.8
     v = 0
-    h = input("Введите высоту: ")
     u = math.sqrt(math.pow(v, 2) + 2 * float(g) * float(h))
     print("Скорость объекта с высоты: " + str(h) + "м при ускорении " + str(g) + "м/с^2 равна: " + str(u))
 
 
-decorator(freefall())
+landheight = input("Введите длину участка в футах: ")
+landwidth = input("Введите ширину участка в футах: ")
+landarea(landheight, landwidth)
+
+
+h = input("Введите высоту: ")
+freefall(h)
